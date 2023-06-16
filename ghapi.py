701acd00e6598c9aecc69cd6500e4454ffa6bf0c
cliff <cliffbakalian@gmail.com>
2023-06-16 02:02:45 -0400

    updated python version

--- ghapi.py
+++ ghapi.py
@@ -2,10 +2,11 @@ import requests
 import json
 import re
 from dotenv import dotenv_values
+from datetime import datetime,timedelta
 
 config = dotenv_values(".env")
 token = config['GITHUB_TOKEN']
-url = config['BASE_URL']
+url = config['BASE_ORG_URL']
 
 headers = {
   "Accept":"application/vnd.github+json",
@@ -16,16 +17,43 @@ parameters = {"per_page":'100'}
 r = requests.head(url, headers=headers, params=parameters)
 pages = int((re.search(r'=(\d+)$', r.links['last']['url']).group(1)))
 
-repos = {}
-for i in range(pages):
-  page = {'page':i+1}
-  page.update(parameters)
-  r = requests.get(url, headers=headers, params=page).json()
-  for repo in r:
-    user = repo['name']
-    checkedout = repo['created_at']
-    repos[user] = checkedout
+time_re = re.compile('(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})T(?P<hour>\d{2}):(?P<minu>\d{2}):(?P<sec>\d{2})Z')
 
-with open("checkout.txt", 'w') as f: 
-    for key, value in repos.items(): 
-        f.write('%s:%s\n' % (key, value))
+OFFSET = 4
+
+def commit_histories(name):
+  comurl = config['BASE_COMMIT_URL']+name+"/commits"
+  r = requests.get(comurl, headers=headers).json()
+  commits = []
+  for commit in r:
+    time = (commit['commit']['author']['date']) 
+    commits.append(str(convertTime(time)))
+  with open(name+".history", 'w') as f: 
+      for time in commits: 
+          f.write('%s:%s\n' % (name, time))
+  return commits
+  
+def convertTime(a):
+  data = re.search(time_re,a)
+  init = datetime(int(data.group('year')),int(data.group('month')),int(data.group('day')),int(data.group('hour')),int(data.group('minu')),int(data.group('sec')))
+  td = timedelta(hours=OFFSET)
+  return init - td
+
+def start():
+  repos = {}
+  for i in range(pages):
+    page = {'page':i+1}
+    page.update(parameters)
+    r = requests.get(url, headers=headers, params=page).json()
+    for repo in r:
+      user = repo['name']
+      checkedout = repo['created_at']
+      # forgot to convert to edt
+      newtime = convertTime(checkedout)      
+      repos[user] = str(newtime)
+
+  with open("checkout.txt", 'w') as f: 
+      for key, value in repos.items(): 
+          f.write('%s:%s\n' % (key, value))
+#start()
+#commit_histories("project-1-CliffBakalian")
